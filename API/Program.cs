fe6dd2821c431a1a2ff21e9dface6a676d8b864c
Ettienne Botes <ettiennebotes2000@gmail.com>
2025-10-30 11:02:54 +0200

    End of  Section 3

--- API/Program.cs
+++ API/Program.cs
@@ -10,10 +10,29 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+        builder =>
+    {
+        builder.WithOrigins("http://localhost",
+            "http://localhost:4200",
+            "https://localhost:7230",
+            "http://localhost:90")
+        .AllowAnyMethod()
+        .AllowAnyHeader()
+        .SetIsOriginAllowedToAllowWildcardSubdomains();
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
+app.UseCors(MyAllowSpecificOrigins);
 app.MapControllers();
 
 app.Run();
