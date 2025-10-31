7a9c5265aa24be3ce5e642a2fca3fa76adc3ad62
Ettienne Botes <ettiennebotes2000@gmail.com>
2025-10-31 14:18:09 +0200

    End of section 4

--- API/Controllers/MemebersController.cs
+++ API/Controllers/MemebersController.cs
@@ -1,6 +1,6 @@
 using API.Data;
 using API.Entities;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -16,11 +16,11 @@ namespace API.Controllers
             var members = await context.Users.ToListAsync();
             return members;
         }
-
+        
         [HttpGet("{id}")]
-        public ActionResult<AppUser> GetMember(string id)
+        public async Task<ActionResult<AppUser>> GetMember(string id)
         {
-            var member = context.Users.Find(id);
+            var member = await context.Users.FindAsync(id);
             if (member == null) return NotFound();
             return member;
         }
